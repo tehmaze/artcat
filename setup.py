2cbe8dd711fe7db16f07390becc5210204ea47f9
Wijnand Modderman-Lenstra <maze@pyth0n.org>
2014-10-15 10:43:18 +0200

    Version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='artcat',
-    version='3.11.1',
+    version='3.12',
     description='A cat(1) like utility for text mode art',
     author='Wijnand Modderman-Lenstra',
     author_email='maze@pyth0n.org',
